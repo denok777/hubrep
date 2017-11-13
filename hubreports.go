5b868cb21c18b139e307691c440a2df82b5e5e7f
bender <denok.777@gmail.com>
2017-11-13 18:58:02 +0300

    List files of reports dir excluding subdirs

--- hubreports.go
+++ hubreports.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	dir := "/tmp/test/"
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		fmt.Println(file.Name())
+	}
+
+	fmt.Println()
+	fmt.Println("ok")
 }
